15ed817411e9317413e557adb97d6684465f34be
Hong Minh <minh.nguyen3@be.com.vn>
2022-03-11 10:36:55 +0700

    backend update backend api ping

---
+++ backend/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	fmt.Println("Hello world")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run()
+}
